b1dca98b34ee7139a55ba011358a7a06820fe3fc
Arikalp <sankalpkrsaini@gmail.com>
2025-06-02 15:00:32 +0530

    added sort

--- Array.java
+++ Array.java
@@ -27,6 +27,40 @@ public class Array {
         return arr; // Return the sorted array
     }
 
+    public static int[] Quick_Sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before and after partition
+            Quick_Sort(arr, low, pi - 1);
+            Quick_Sort(arr, pi + 1, high);
+        }
+        return arr; // Return the sorted array
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; // pivot
+        int i = (low - 1); // Index of smaller element
+
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than or equal to pivot
+            if (arr[j] <= pivot) {
+                i++; // increment index of smaller element
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Swap arr[i + 1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1; // Return the partitioning index
+    }
+
     public static int[] Merge_Sort(int[] arr, int left, int right) {
         if (left < right) {
             int mid = (left + right) / 2;
